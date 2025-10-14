e200a24dc868e108c8e9d6bb2dd24718bd694601
Aditya m vadavadagi <124175912+adityamvMCA@users.noreply.github.com>
2025-10-14 19:58:28 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Scan all JS/TS/JSX/TSX files in src
+    "./public/index.html"           // Optional: scan your main HTML file
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
